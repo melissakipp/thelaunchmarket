1361f28d06638f3b5ba37b766c0fb2dbc789e9fb
Christer Johansson <christer.johansson@stenungsund.nu>
2023-03-25 14:20:26 +0100

    :sparkles: new(ho-reload-docker) Adds hot reload feature. Adds meta data to application

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className={styles.main}>
-      <h1>The Launch Market</h1>
+      <h1>The Launch Market v.0.1.1 dev</h1>
+      <p>With hot reloading enabled in Docker</p>
+      <a href="Go To Page" onClick={() => router.push("/page")}>
+        Back to Home Page
+      </a>
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
