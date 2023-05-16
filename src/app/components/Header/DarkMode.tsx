4d73e429256561a7a0143d20c23df222748a5b09
Melissa Kipp <melissakipp.az@gmail.com>
2023-05-16 13:09:45 -0700

    {hot fix} removed comments

--- src/app/components/Header/DarkMode.tsx
+++ src/app/components/Header/DarkMode.tsx
@@ -17,21 +17,12 @@ export default function DarkMode() {
 
   return (
     <div className='containerToggles'>
-      {/* <label className='srOnly' htmlFor='theme-mode'>Choose a theme:</label>
-      <select id='theme-mode' value={theme} onChange={(e) => setTheme(e.target.value)}>
-        <option value='system'>System</option>
-        <option value='light'>Light</option>
-        <option value='dark'>Dark</option>
-      </select> */}
-
        <div className="flex justify-center">
             {currentTheme === 'dark' ? (
               <button
                 className="toggle"
                 onClick={() => setTheme('light')}
               >
-                {/* {' '}
-                <Image src="/sun.svg" alt="Sun icon for light mode" height={18} width={18} /> */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
               </button>
             ) : (
@@ -39,28 +30,10 @@ export default function DarkMode() {
                 className="toggle"
                 onClick={() => setTheme('dark')}
               >
-                {/* <Image src="/moon.svg" alt="Moon icon for dark mode" height={18} width={18} /> */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
               </button>
             )}
           </div>
-
-      {/* <p className='srOnly'>Current theme:{theme}</p>
-      <button className='toggle' onClick={() => setTheme('light')}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
-        <span className='srOnly'>
-          Light Mode
-        </span>
-      </button>&nbsp;&nbsp;
-      <button className='toggle' onClick={() => setTheme('dark')}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-        <span className='srOnly'>
-          Dark Mode
-        </span>
-      </button>&nbsp;&nbsp; */}
-      {/* <button onClick={() => setTheme('system')}>System</button> */}
-
-      {/* <button className='toggle' role='switch' aria-label='light/dark mode'></button> */}
       <audio src='audio/light-on.mp3' className='themeAudio__lightOn' data-attribute='adapted from user 160033 file on freesound.org https://freesound.org/people/160033/sounds/366184/ under CC BY-NC 3.0'></audio>
       <audio src='audio/light-off.mp3' className='themeAudio__lightOff' data-attribute='adapted from user 160033 file on freesound.org https://freesound.org/people/160033/sounds/366184/ under CC BY-NC 3.0'></audio>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 'use client';
-// import React, { useEffect, useState } from 'react';
 import head from 'next/head';
-// Must add 'use client' for the ThemeProvider to work
 import { ThemeProvider } from 'next-themes';
 
 import './styles/globals.css';
@@ -14,23 +12,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
-  // const { systemTheme, theme, setTheme } = useTheme();
-  // const [mounted, setMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setMounted(true);
-  // }, []);
-
-  // if (!mounted) return null;
-  // const currentTheme = theme === 'system' ? systemTheme : theme;
-
   return (
     
     <html lang="en">
       <head />
       <body>
-        {/* when added attribute='class' functionality broke */}
         <ThemeProvider>
           <Header />
           {children}
