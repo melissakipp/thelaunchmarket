bbadbca4a3e4dd8f6989fe941802770f27c0032f
Melissa Kipp <melissakipp.az@gmail.com>
2023-05-16 18:30:31 -0700

    Restructured styles files

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,12 @@
-import styles from '../../styles/Header.module.css';
+import DarkModeToggle from './DarkMode';
+
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div>        
-          <p>The Launch Market</p>
+        <DarkModeToggle />
       </div>
 
     </header>
