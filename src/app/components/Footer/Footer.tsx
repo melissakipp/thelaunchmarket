6170c3bdbd21597ba417a7ae6e6273d19183853b
Melissa Kipp <melissakipp.az@gmail.com>
2023-06-24 10:22:44 -0700

    Footer content edit

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,9 +3,14 @@ import styles from '../../../styles/Footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <h3>The Launch Market &copy; 2023</h3>
-      <p>&#128386; <a href="mailto:info@thelaunchmarket.com ">Contact Us</a></p>
-      <small>Created with &#10084; <a href="https://melissajkipp.com">Melissa Kipp</a></small>
+      <h3>Launch Market &copy; 2023</h3>
+      <p>
+        &#128386; <a href='mailto:info@thelaunchmarket.com '>Contact Us</a>
+      </p>
+      <small>
+        Created with &#10084;{' '}
+        <a href='https://melissajkipp.com'>Melissa Kipp</a>
+      </small>
     </footer>
   );
 }
