e1d435354d39a30fce02e8fcdc821454b465d33f
Melissa Kipp <melissakipp.az@gmail.com>
2023-06-26 07:30:32 -0700

    Basic button component

--- src/app/components/UI/Button.tsx
+++ src/app/components/UI/Button.tsx
@@ -1,11 +1,30 @@
-import { FC } from 'react';
+type Props = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {};
 
-interface ButtonProps {
-  children: React.ReactNode;
-}
-
-const Button: FC<ButtonProps> = ({ children }) => {
-  return <button>{children}</button>;
+const Button = (props: Props) => {
+  return <button {...props} />;
 };
 
 export default Button;
+
+// const Button = styles.button`
+//   fontsize: 1rem;
+//   padding: 0.5rem 1rem;
+//   border: 0;
+//   background-color: #000;
+//   color: #fff;
+//   border-radius: 0.25rem;
+//   cursor: pointer;
+//   box-shadow: var(--borderShadow);
+//   transition: 300ms;
+//   &:hover {
+//     background-color: dodgerblue;
+//     opacity: 0.8;
+//   }
+
+//   &:active {
+//     transform: scale(0.95);
+//   }
+// `;
