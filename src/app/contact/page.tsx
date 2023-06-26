d2d1af8a5099aa592810405c7f2edb355436dc3a
Melissa Kipp <melissakipp.az@gmail.com>
2023-06-26 09:07:06 -0700

    Basic contact icons

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,12 @@
 import styles from '../../styles/Contact.module.scss';
 import type { Metadata } from 'next';
 import Form from '../components/Form/Form';
+import ListWithIcon from '../components/List/ListWithIcon';
+import {
+  BiCalendar,
+  BiSolidEnvelope,
+  BiLogoLinkedinSquare,
+} from 'react-icons/bi';
 
 export const metadata: Metadata = {
   title: 'Contact',
@@ -10,8 +16,26 @@ export const metadata: Metadata = {
 export default function Contact() {
   return (
     <main className={styles.main}>
-      <article>
-        <section></section>
+      <article className={styles.container}>
+        <section>
+          <ul className={styles.list}>
+            <ListWithIcon
+              name='Calendar'
+              icon={<BiCalendar />}
+              href='https://calendly.com/melissakipp/15-minute-chat'
+            />
+            <ListWithIcon
+              name='Email'
+              icon={<BiSolidEnvelope />}
+              href='info@thelaunchmarket.com'
+            />
+            <ListWithIcon
+              name='LinkedIn'
+              icon={<BiLogoLinkedinSquare />}
+              href='https://www.linkedin.com/company/launchmarket/'
+            />
+          </ul>
+        </section>
         <section>
           <Form />
         </section>
