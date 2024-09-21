ed0f9e21cd679a0fa6865664d087d5270715157b
melissakipp <melissakipp.az@gmail.com>
2024-09-21 08:25:32 -0700

    Add services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Services | The Launch Market',
+  description: 'Welcome to The Launch Market',
+};
+
+export default function Services() {
+  return (
+    <article>
+      <h1>Our Services at The Launch Market</h1>
+      At Launch Market, we'e dedicated to helping your business thrive with tailored design, development, and marketing solutions. Whether you're looking to build a new website, refine your branding, or launch a targeted marketing campaign, our team is here to guide you every step of the way. Our proven process ensures clear communication, high-quality results, and a seamless experience from start to finish. Learn more about our services below, and get in touch to discuss how we can help you achieve your goals. 
+    </article>
+  )
+}
