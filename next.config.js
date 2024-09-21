325bdddbbb14efc3a518f0c3adab4553f5d63bf2
melissakipp <melissakipp.az@gmail.com>
2024-09-21 08:59:26 -0700

    Add configuration for the alias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
+
+const path = require('path');
 
 module.exports = nextConfig;
