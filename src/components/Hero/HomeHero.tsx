9376e1ae19770ca2c64bc1cc05fd11d30c9e123a
melissakipp <melissakipp.az@gmail.com>
2024-11-30 10:21:53 -0700

    Final verson for home hero section

--- src/components/Hero/HomeHero.tsx
+++ src/components/Hero/HomeHero.tsx
@@ -1,20 +1,19 @@
-import Link from 'next/link';
-
 import { FiArrowRight } from 'react-icons/fi';
 import styles from './HomeHero.module.css';
+import CustomBtnLink from '../UI/Links/CustomBtnLink';
+import CustomLink from '../UI/Links/CustomBtnLink';
+
 
 export default function HomeHero() {
   return (
     <section className={styles.hero}>
-        <h1>Transforming Ideas into Digital Reality</h1>
-        <p>Beautiful, functional websites that grow with your business. No complexity, just results.</p>
-        <Link
-          href='/contact'
-          className={styles.primaryLink}
-        >
-          Start Your Project 
-          <FiArrowRight />
-        </Link>
-      </section>
+      <h1 className='srOnly'>The Launch Market's home page</h1>
+      <h2>Modern Web Design for <br />Your Growing Brand</h2>
+      <p>Beautiful, functional websites built for your future. No complexity, just results.</p>
+      {/* Outline Button */}
+      <CustomBtnLink href="/about" variant="outline" size="sm">
+        Get Started
+      </CustomBtnLink>
+    </section>
   );
 }
