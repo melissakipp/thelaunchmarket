e16480c75619dd57c0bc1571e78d83fef24cd4e8
melissakipp <melissakipp.az@gmail.com>
2024-12-01 06:18:33 -0700

    Adjust spacing and font size

--- src/components/Hero/HomeHero.tsx
+++ src/components/Hero/HomeHero.tsx
@@ -8,7 +8,7 @@ export default function HomeHero() {
     <section className={styles.hero}>
       <h1 className='srOnly'>The Launch Market's home page</h1>
       <h2>Modern Web Design for <br />Your Growing Brand</h2>
-      <p>Beautiful, functional websites built for your future. No complexity, just results.</p>
+      <p>Beautiful, functional websites built for your future. <br />No complexity, just results.</p>
       {/* Outline Button */}
       <CustomBtnLink 
         href='/about'
